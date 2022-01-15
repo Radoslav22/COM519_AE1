643b93033722b4b78954464933a0f0dabfe81e86
Radoslav22 <4takvr26@solent.ac.uk>
2022-01-15 12:00:12 +0000

    Successfully reading the data

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
 const { PORT, MONGODB_URI } = process.env;
-const {MongoClient} = require ("mongodb");
+const { MongoClient } = require("mongodb");
+const Price = require("./models/Pricing");
 
 
 const pricingController = require("./controllers/pricing");
+const readController = require("./controllers/read");
+
+const uri = MONGODB_URI
+
+
+const client = new MongoClient(uri);
 
 
 
@@ -17,57 +24,45 @@ const pricingController = require("./controllers/pricing");
 
 
 
-
-
-async function main(){
+//async function main() {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-  const uri = MONGODB_URI
+  //const uri = MONGODB_URI
 
 
-  const client = new MongoClient(uri);
+  //const client = new MongoClient(uri);
 
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
+  //try {
+    // Connect to the MongoDB cluster
+    //await client.connect();
 
-      // Make the appropriate DB calls
-      //await createListing(client,)
-      await findOneListingByName(client,"BMW");
+    // Make the appropriate DB calls
+    //await createListing(client,)
+    //await exports.list(client, "BMW");
+    
 
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
-}
+  //} catch (e) {
+    //console.error(e);
+ // } finally {
+   // await client.close();
+ // }
+//};
 
+ 
+//main().catch(console.error);
+//async function listDatabases(client){
+//databasesList = await client.db().admin().listDatabases();
 
-
-main().catch(console.error);
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-
+//console.log("Databases:");
+//databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+//};
 
 
-async function findOneListingByName(client, nameOfListing) {
-  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-  const result = await client.db("COM519").collection("bmw_pricing_challenge").findMany({ maker_key: nameOfListing });
-  
-  if (result) {
-      console.log(`Found a listing in the collection with the name '${nameOfListing}':`);
-      console.log(result);
-  } else {
-      console.log(`No listings found with the name '${nameOfListing}'`);
-  }
-}
+
+
+
 
 
 
@@ -79,7 +74,7 @@ async function findOneListingByName(client, nameOfListing) {
 
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(expressSession({ secret: 'foo barr', cookie: { expires: new Date(253402300000000) } }))
 
@@ -96,15 +91,17 @@ mongoose.connection.on("error", (err) => {
 });
 
 
-app.get("/",(req,res)=>{
-    res.render("index")
+app.get("/", (req, res) => {
+  res.render("index")
 });
-
-app.get("/pricing", pricingController.list);
-
+//main()
+//app.get("/pricing",(req,res)=>{
+  //res.render(qko);
+//});
+app.get("/pricing",pricingController.list );
 app.listen(PORT, () => {
-    console.log(
-        `Example app listening at http://localhost:${PORT}`,
-        //chalk.green("✓")
-    );
+  console.log(
+    `Example app listening at http://localhost:${PORT}`,
+    //chalk.green("✓")
+  );
 });
--- controllers/pricing.js
+++ controllers/pricing.js
@@ -1,11 +1,21 @@
 const Pricing = require("../models/Pricing");
+const {PORT, MONGODB_URI} = process.env;
+const {MongoClient} = require("mongodb");
+const uri = MONGODB_URI
+const client = new MongoClient(uri);
+client.connect();
 
 exports.list = async (req, res) => {
     try {
-      const pricings = Pricing.find({});
-      console.log(pricings);
-      res.render("pricing", { pricings: pricings });
+      const cursor = client.db("COM519").collection("bmw_pricing_challenge").find({ model_key: "420" })
+      //console.log(pricings);
+      //const pricings = Pricing.find({maker_key:"BMW"})
+      //const cursor = collection.find({});
       
+      const firstResult = await cursor.toArray();
+      console.log(firstResult);
+      res.render("pricing",{firstResult: firstResult})
+      //res.send({message:"list prices"})
     } catch (e) {
       res.status(404).send({ message: "could not list prices" });
     }
